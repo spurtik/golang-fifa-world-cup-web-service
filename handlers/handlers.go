273aac544cfe3382cc91972f2205570a17df9216
Spurti <spurtik@users.noreply.github.com>
2022-07-01 16:08:49 +1000

    First commit

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,12 +1,30 @@
 package handlers
 
 import (
+	"golang-fifa-world-cup-web-service/data"
 	"net/http"
 )
 
 // RootHandler returns an empty body status code
 func RootHandler(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	year := req.URL.Query().Get("year")
+	if year == "" {
+		winners, err := data.ListAllJSON()
 
+		if err != nil {
+			res.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		res.Write(winners)
+	} else {
+		filteredWinners, err := data.ListAllByYear("year")
+		if err != nil {
+			res.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		res.Write(filteredWinners)
+	}
 }
 
 // ListWinners returns winners from the list
